262e48d9a61231d7c6fcf7b7509d689b16cf1783
GilgJon <35938198+GilgJon@users.noreply.github.com>
2018-03-20 11:43:57 -0700

    Ranger updates

--- ECE 266 Labs/Lab7/ranger.asm
+++ ECE 266 Labs/Lab7/ranger.asm
@@ -14,10 +14,10 @@
 ; GPIO peripheral, port base and pin mask value for the buzzer
 ; The buzzer is assumed to be connected to J17, andthen the buzzer's
 ; signal pin is connected to PC4. 
-ROTARY_PERIPH   .field  SYSCTL_PERIPH_ADC0
-ROTARY_PORT     .field 	ADC0_BASE
-ROTARY_PIN      .equ	GPIO_PIN_3
-ROTARY_Trig		.field  ADC_TRIGGER_PROCESSOR
+RANGER_PERIPH   .field  SYSCTL_PERIPH_WTIMER0
+RANGER_PORT     .field 	GPIO_PORTC_BASE
+RANGER_PIN      .equ	GPIO_PIN_5
+RANGER_Trig		.field  ADC_TRIGGER_PROCESSOR
 
 ;
 ; void rangerInit(): Initialze the rotary peripheral, port, and pin direction
@@ -25,58 +25,79 @@ ROTARY_Trig		.field  ADC_TRIGGER_PROCESSOR
 rangerInit      PUSH 	{LR}
 				
 				; Initialize the GPIO peripheral for the port that the rotary uses:
-                ;   Call SysCtlPeripheralEnable(ROTARY_PERIPH)
-                LDR   r0, ROTARY_PERIPH
-                BL    SysCtlPeripheralEnable
+                ;   Call SysCtlPeripheralEnable(RANGER_PERIPH)
+                LDR   	r0, RANGER_PERIPH
+                BL    	SysCtlPeripheralEnable
 				
 				; Preip
-loop			LDR  r0, ROTARY_PERIPH
-				BL   SysCtlPeripheralReady
-				TEQ  r0, #1
-				BNE  loop
+loop			LDR  	r0, RANGER_PERIPH
+				BL   	SysCtlPeripheralReady
+				TEQ  	r0, #1
+				BNE  	loop
 
 				; Configure ADC sequencer #0
-				LDR	  r0, ROTARY_PORT
-				MOV   r1, #0
-				LDR   r2, ROTARY_Trig
-				MOV   r3, #0
-				BL    ADCSequenceConfigure
+				LDR	  	r0, ROTARY_PORT
+				MOV   	r1, #0
+				LDR  	r2, ROTARY_Trig
+				MOV   	r3, #0
+				BL    	TimerConfigure
+				
+				; Configure event
+				LDR	  	r0, ROTARY_PORT
+				MOV   	r1, #0
+				LDR   	r2, ROTARY_Trig
+				MOV   	r3, #0
+				LDR   	r4, TIMER_EVENT_POS_EDGE
+				MOV   	r5, #0
+				BL	  	TimerControlEvent
+				
+				; Time Trig configure
+				LDR	  	r0, ROTARY_PORT
+				MOV   	r1, #0
+				LDR  	r2, ROTARY_Trig
+				MOV   	r3, #0
+				BL    	TimerEnable
+				
+				; Interpreting timer status
+				LDR	  	r0, ROTARY_PORT
+				MOV   	r1, #0
+				LDR  	r2, #false
+				MOV   	r3, #0
+				BL		TimerIntStatus
 				
 				; Configure a single step of the sequencer
-				LDR	  r0, ROTARY_PORT
-				MOV   r1, #0
-				MOV   r2, #0
-				MOV   r3, #(ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH2)
-				BL    ADCSequenceStepConfigure
+loop_two		MOV     r0, #100
+				BX		waitUS			; Time delay for 100 milsec
+				LDR	  	r0, ROTARY_PORT
+				MOV   	r1, #0
+				LDR	  	r0, RANGER_Trig
+				MOV   	r1, #0
+				BL    	TimerIntClear
+				TEQ  	r0, #1
+				BNE  	loop_two
 
-                ; Set the GPIO pin for the motion as input type:
-                ;   Call GPIOPinTypeGPIOInput(ROTARY_PORT, ROTARY_PIN)
-                LDR   r0, ROTARY_PORT
-                MOV   r1, #0
-                BL    ADCSequenceEnable
-
-                POP   {PC}
+                POP   	{PC}
 				
 
-rangerGet   	PUSH  {LR}		; save return address
+rangerGet   	PUSH  	{LR}		; save return address
 				
 				; Trigger
-				LDR   r0, ROTARY_PORT
-				MOV   r1, #0
-				BL	  ADCProcessorTrigger
+				LDR   	r0, ROTARY_PORT
+				MOV   	r1, #0
+				BL	  	ADCProcessorTrigger
 				
 				; Wait until the sample sequence has completed
-while_loop		LDR	  r0, ROTARY_PORT
-				MOV	  r1, #0
-				MOV   r2, #0
-				BL    ADCIntStatus
-				CMP  r0, #0
-				BEQ  while_loop
+loop_three		LDR	  	r0, ROTARY_PORT
+				MOV	 	r1, #0
+				MOV   	r2, #0
+				BL    	ADCIntStatus
+				CMP  	r0, #0
+				BEQ  	loop_three
 				
-				LDR   r0, ROTARY_PORT
-				MOV   r1, #0
-				SUB   SP, #4
-				MOV   r2, sp ; addr of adcReading is sp
-				BL    ADCSequenceDataGet
+				LDR   	r0, ROTARY_PORT
+				MOV   	r1, #0
+				SUB   	SP, #4
+				MOV   	r2, sp ; addr of adcReading is sp
+				BL    	ADCSequenceDataGet
 
-				POP {r0, pc} ; pop adcReading to r0 and return
+				POP 	{r0, pc} ; pop adcReading to r0 and return
