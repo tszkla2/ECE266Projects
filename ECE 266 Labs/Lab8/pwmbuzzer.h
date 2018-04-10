77cc79cc068cc28373bd4f2198e81edd096fd828
GilgJon <35938198+GilgJon@users.noreply.github.com>
2018-04-09 18:53:24 -0700

    Part 1

--- ECE 266 Labs/Lab8/pwmbuzzer.h
+++ ECE 266 Labs/Lab8/pwmbuzzer.h
@@ -1,28 +1,14 @@
-/*
- * pwdled.h: for Lab 8, spring 2018
- *
- *  Created by Zhao Zhang
- */
-
-#ifndef PWMLED_H_
-#define PWMLED_H_
-
-#include <stdint.h>
-
-// The PWM parameters
-typedef struct {
-    uint16_t pulse_width;       // PWM pulse width
-    uint16_t period;            // PWM period in cycles
-} pwm_t;
+#ifndef PWMBUZZER_H_
+#define PWMBUZZER_H_
 
 /*
- * Initialize the timer PWM functions connected to the three sub-LEDs.
+ * Initialize the timer and buzzer.
  */
-void pwmledInit();
+void buzzerInit();
 
 /*
- * Set color of the on-board LED by setting the intensity of the primary colors
+ * play buzzer
  */
-void pwmledSetColor(pwm_t red, pwm_t green, pwm_t blue);
+void buzzerOn(uint32_t type);
 
 #endif /* PWMLED_H_ */
