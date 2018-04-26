cd4fd064838d8ef3db2b559389447b659ebab688
GilgJon <35938198+GilgJon@users.noreply.github.com>
2018-04-25 17:09:52 -0700

    light fix

--- ECE 266 Labs/Final Project/light.c
+++ ECE 266 Labs/Final Project/light.c
@@ -14,11 +14,6 @@ void lightInit()
 	// ---Initialize the GPIO peripheral for the port that the light sensor uses to function
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 	
-	// --Waiting on device
-	while(SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0) != 1){
-		// Empty loop
-	}
-	
 	// --Configure ADC sequencer #0
 	ADCSequenceConfigure(GPIO_PIN_3, 0, ADC_TRIGGER_PROCESSOR,0);
 	
@@ -35,23 +30,18 @@ void lightInit()
 uint32_t lightGet()
 {
 	// ---Variables
-	int i = 0;			// Checking for loop interrupt by pass of read value
+	uint32_t readingValue;		// Return value
 	
-	// ---Getting the Value of light sensor to return
-	// --Wait for ADC to trigger
-	ADCProcessorTrigger(GPIO_PIN_3, 0);
+    // ---Getting the Value of light sensor to return
+	// --ADC trigger
+    ADCProcessorTrigger(ADC0_BASE, 0);
 	
-	// --Conducting Sampling
+    // --Conducting Sampling
 	// -Checking for interrupt
-	while(i == 0){
-		i = ADCIntStatus(GPIO_PIN_3, 0, 0);
-	}
+    while (!ADCIntStatus(ADC0_BASE, 0, false)) {
+    }
 	
-	// -Getting read value and return it (done ASM as c version doesn't work)
-	__asm__("LDR   r0, ROTARY_PORT\n\t"
-			"MOV   r1, #0\n\t"
-			"SUB   SP, #4\n\t"
-			"MOV   r2, sp\n\t"
-			"BL    ADCSequenceDataGet\n\t"
-			"POP {r0, pc}\n\t");  //pop adcReading to r0 and return
+    // -Getting read value and return it
+    ADCSequenceDataGet(ADC0_BASE, 0, &readingValue);
+    return (uint32_t) readingValue;
 }
