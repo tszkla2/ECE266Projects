0c2453c6010fd26522cb90fc7b81ea21b313c510
tszkla2 <tszkla2@uic.edu>
2018-04-30 18:03:02 -0500

    FINAL PROJECT

--- ECE 266 Labs/Final Project/main.c
+++ ECE 266 Labs/Final Project/main.c
@@ -11,11 +11,14 @@
 #include <driverlib/sysctl.h>
 #include "launchpad.h"
 #include "pwmBuzzer.h"
+#include "light.h"
 
 //static enum {Run, Pause}
 //sysState = Pause;
 uint8_t sysStateLed;
 uint8_t sysStateBuz;
+uint8_t currSong = 0;
+int counts = 0;
 
 /*
  * PWM table for gradual changes.
@@ -51,20 +54,20 @@ pwmSeg_t pwmTable [] = {
 
 pwmB_t intensityBuzzerTable[] = {
 
-  {227272, 1000}, // A3 - 0
-  {202478, 1000}, // B3 - 1
-  {191112, 1000}, // C4 - 2
-  {170264, 1000}, // D4 - 3
-  {151685, 1000}, // E4 - 4
-  {143172, 1000}, // F4 - 5
-  {127552, 1000}, // G4 - 6
-  {113636, 1000}, // A4 - 7
-  {101238, 1000}, // B4 - 8
-  {95556, 1000},  // C5 - 9
-  {85131, 1000},  // D5 - 10
-  {75843, 1000},  // E5 - 11
-  {71586, 1000},  // F5 - 12
-  {63776, 1000},  // G5 - 13
+  {227272, 15000}, // A3 - 0
+  {202478, 15000}, // B3 - 1
+  {191112, 15000}, // C4 - 2
+  {170264, 15000}, // D4 - 3
+  {151685, 15000}, // E4 - 4
+  {143172, 15000}, // F4 - 5
+  {127552, 15000}, // G4 - 6
+  {113636, 15000}, // A4 - 7
+  {101238, 15000}, // B4 - 8
+  {95556, 15000},  // C5 - 9
+  {85131, 15000},  // D5 - 10
+  {75843, 15000},  // E5 - 11
+  {71586, 15000},  // F5 - 12
+  {63776, 15000},  // G5 - 13
   {100, 1}        // Blank - 14
 };
 
@@ -83,14 +86,21 @@ checkPushButton(uint32_t time)
 	        sysStateLed = 1;
 	    else if(sysStateLed == 1)
 	        sysStateLed = 0;
+	    currSong++;
+		counts = 0;
+	    if(currSong == 2)
+	        currSong = 0;
+	    buzzerOn(intensityBuzzerTable[14].pwmPeriod, intensityBuzzerTable[14].pwmPeriod - intensityBuzzerTable[14].pwmDutyCycle);
 		delay = 250;
 		break;
 
 	case 2:
 	    if(sysStateBuz == 0)
 	        sysStateBuz = 1;
-	    else if(sysStateBuz == 1)
+	    else if(sysStateBuz == 1){
 	        sysStateBuz = 0;
+            buzzerOn(intensityBuzzerTable[14].pwmPeriod, intensityBuzzerTable[14].pwmPeriod - intensityBuzzerTable[14].pwmDutyCycle);
+	    }
 	    delay = 250;
 		break;
 
@@ -107,22 +117,23 @@ checkPushButton(uint32_t time)
 void
 pwmledUpdate(uint32_t time)
 {
-    static enum color_t {RED = 0, GREEN = 1, BLUE = 2} color = RED;     // Current color to display
-    static int seg = 0;                                                 // Current segment of PWM settings
+    static enum color_t {RED = 0, GREEN = 1, BLUE = 2} color = BLUE;     // Current off state
+    static int seg = 4;                                                 // Current segment of PWM settings
+    int lightSens = lightGet();                                         // Current Light sensor state
 
     if (sysStateLed == 1) {
         // Set up PWM parameters for the three sub-LEDs. Only one LED is lit at a time.
         pwm_t off = {0, 200};
+        if(lightSens >= 2000){   // Day time
+            color = RED;
+        }
+        else{
+            color = BLUE;
+        }
         pwm_t red = (color == RED) ? pwmTable[seg].pwm : off;
-        pwm_t green = (color == GREEN) ? pwmTable[seg].pwm : off;
+        pwm_t green = off;
         pwm_t blue = (color == BLUE) ? pwmTable[seg].pwm : off;
         pwmledSetColor(red, green, blue);
-
-        // Move to the next segment, switch color on warp-around
-        seg = (seg + 1) % LEGS;
-        if (seg == 0) {
-            color = (enum color_t) ((color + 1) % 3);
-        }
 
         schdCallback(pwmledUpdate, time + pwmTable[seg].duration);
     }
@@ -132,23 +143,67 @@ pwmledUpdate(uint32_t time)
     }
 }
 
-int song[]= {4,14,3,14,2,14,3,14,4,14,4,14,4,14,3,14,3,14,3,14,4,14,6,14,6,14,4,14,3,14,2,14,3,14,4,14,4,14,4,14,4,14,3,14,3,14,4,14,3,14,2,14,14,14}; // put song here
-int counts = 0;
-int song2[]= {0,14,3,14,3,14,3,14,4,14,5,14,5,14,5,14,4,14,3,14,4,14,5,14,3,14,5,14,5,14,6,14,7,14,7,14,6,14,5,14,6,14,7,14,5,14,3,14,3,14,4,14,5,14,5,14,4,14,3,14,4,14,5,14,3,14,0,14,0,14,3,14,3,14,3,14,4,14,5,14,5,14,5,14,4,14,3,14,4,14,5,14,3,14,14,14};
-int song3[]= {3,14,6,14,6,14,3,14,3,14,4,14,4,14,3,14,3,14,6,14,6,14,7,14,7,14,8,14,6,14,8,14,8,14,9,14,9,14,9,14,7,14,7,14,8,14,8,14,8,14,6,14,6,14,7,14,7,14,7,14,6,14,5,14,3,14,4,14,5,14,6,14,6,14,14,14};
-int song4[]= {3,14,3,14,5,14,5,14,4,14,5,14,4,14,3,14,5,14,5,14,7,14,7,14,6,14,7,14,6,14,5,14,8,14,8,14,8,14,7,14,7,14,7,14,8,14,8,14,8,14,7,14,6,14,6,14,6,14,5,14,5,14,5,14,4,14,5,14,4,14,3,14,14,14};
-int song5[]= {1,14,3,14,8,14,7,14,6,14,1,14,3,14,6,14,5,14,2,14,3,14,9,14,6,14,7,14,7,14,6,14,4,14,3,14,1,14,3,14,8,14,7,14,6,14,1,14,3,14,6,14,5,14,2,14,6,14,9,14,8,14,6,14,7,14,4,14,5,14,6,14,14,14};
+// ---LIST OF SONGS:
+int song[]= {4,14,3,14,2,14,3,14,4,14,4,14,4,14,3,14,3,14,3,14,4,14,6,14,6,14,4,14,3,14,2,14,3,14,4,14,4,14,4,14,4,14,3,14,3,14,4,14,3,14,2,14,14,14}; //Mary Had 54
+//int song2[]= {0,14,3,14,3,14,3,14,4,14,5,14,5,14,5,14,4,14,3,14,4,14,5,14,3,14,5,14,5,14,6,14,7,14,7,14,6,14,5,14,6,14,7,14,5,14,3,14,3,14,4,14,5,14,5,14,4,14,3,14,4,14,5,14,3,14,0,14,0,14,3,14,3,14,3,14,4,14,5,14,5,14,5,14,4,14,3,14,4,14,5,14,3,14,14,14};// Spider
+int song3[]= {3,14,6,14,6,14,3,14,3,14,4,14,4,14,3,14,3,14,6,14,6,14,7,14,7,14,8,14,6,14,8,14,8,14,9,14,9,14,9,14,7,14,7,14,8,14,8,14,8,14,6,14,6,14,7,14,7,14,7,14,6,14,5,14,3,14,4,14,5,14,6,14,6,14,14,14}; // Bingo 76
+//int song4[]= {3,14,3,14,5,14,5,14,4,14,5,14,4,14,3,14,5,14,5,14,7,14,7,14,6,14,7,14,6,14,5,14,8,14,8,14,8,14,7,14,7,14,7,14,8,14,8,14,8,14,7,14,6,14,6,14,6,14,5,14,5,14,5,14,4,14,5,14,4,14,3,14,14,14}; // Humpty
+//int song5[]= {1,14,3,14,8,14,7,14,6,14,1,14,3,14,6,14,5,14,2,14,3,14,9,14,6,14,7,14,7,14,6,14,4,14,3,14,1,14,3,14,8,14,7,14,6,14,1,14,3,14,6,14,5,14,2,14,6,14,9,14,8,14,6,14,7,14,4,14,5,14,6,14,14,14}; //Rockabye
+int song6[]={2,14,2,14,6,14,6,14,7,14,7,14,6,14,5,14,5,14,4,14,4,14,3,14,3,14,2,14,6,14,6,14,5,14,5,14,4,14,4,14,3,14,6,14,6,14,5,14,5,14,4,14,4,14,3,14,2,14,2,14,6,14,6,14,7,14,7,14,6,14,5,14,5,14,4,14,4,14,3,14,3,14,2,14,14,14}; //Twinkle 86
+int song7[]={4,14,4,14,4,14,4,14,4,14,4,14,4,14,6,14,2,14,3,14,4,14,5,14,5,14,5,14,5,14,5,14,4,14,4,14,4,14,4,14,3,14,3,14,4,14,3,14,6,14,4,14,4,14,4,14,4,14,4,14,4,14,4,14,6,14,2,14,3,14,4,14,5,14,5,14,5,14,5,14,5,14,4,14,4,14,4,14,6,14,6,14,5,14,3,14,2,14,14,14}; //Jingle 98
+
 void soundUpdate(uint32_t time)
 {
-	
+	int lightSens = lightGet();
 	if(sysStateBuz == 1){
-		buzzerOn(intensityBuzzerTable[song5[counts]].pwmPeriod, intensityBuzzerTable[song5[counts]].pwmPeriod - intensityBuzzerTable[song5[counts]].pwmDutyCycle);
-		counts++;
-		if(counts == 75)
-		{
-		    counts = 0;
-		}
-		schdCallback(soundUpdate, time + 300);
+	 if(lightSens >= 2000){   // Day time
+	    if(currSong == 0){
+	        buzzerOn(intensityBuzzerTable[song3[counts]].pwmPeriod, intensityBuzzerTable[song3[counts]].pwmPeriod - intensityBuzzerTable[song3[counts]].pwmDutyCycle);
+	        counts++;
+	        if(counts == 75)
+	        {
+	            counts = 0;
+	        }
+	        schdCallback(soundUpdate, time + 300);
+	    }
+	    else if(currSong == 1){
+	        buzzerOn(intensityBuzzerTable[song7[counts]].pwmPeriod, intensityBuzzerTable[song7[counts]].pwmPeriod - intensityBuzzerTable[song7[counts]].pwmDutyCycle);
+	        counts++;
+	        if(counts == 98)
+	        {
+	            counts = 0;
+	        }
+	        schdCallback(soundUpdate, time + 300);
+	    }
+	    else
+	    {
+	        schdCallback(soundUpdate, time + 200);
+	    }
+	  }
+	  else{   // Night time
+	    if(currSong == 0){
+	        buzzerOn(intensityBuzzerTable[song[counts]].pwmPeriod, intensityBuzzerTable[song[counts]].pwmPeriod - intensityBuzzerTable[song[counts]].pwmDutyCycle);
+	        counts++;
+	        if(counts == 53)
+	        {
+	            counts = 0;
+	        }
+	        schdCallback(soundUpdate, time + 300);
+	    }
+	    else if(currSong == 1){
+	        buzzerOn(intensityBuzzerTable[song6[counts]].pwmPeriod, intensityBuzzerTable[song6[counts]].pwmPeriod - intensityBuzzerTable[song6[counts]].pwmDutyCycle);
+	        counts++;
+	        if(counts == 85)
+	        {
+	            counts = 0;
+	        }
+	        schdCallback(soundUpdate, time + 300);
+	    }
+	    else
+	    {
+            schdCallback(soundUpdate, time + 200);
+	    }
+	  }
 	}
 	else
 	{
@@ -174,13 +229,12 @@ void soundUpdate(uint32_t time)
 	// G5 (783.99) = (63776.324)
 }
 
-// {
 void main(void)
 {
 	lpInit();
 	pwmledInit();
 	buzzerInit();
-
+	lightInit();
 	uprintf("%s\n\r", "Lab 8: Music Player");
 
 
